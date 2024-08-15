537c6f53ea7933d545e9f5dfbcbd657bb3138235
Vlad Romanov <vladromeur@gmail.com>
2024-08-15 14:05:05 +0500

    Завершил вторую главу
    Добавил форму. Добавил ngModel для имени героя.

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {Hero} from "../../types/interfaces/hero.interface";
 import {UpperCasePipe} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
   imports: [
-    UpperCasePipe
+    UpperCasePipe,
+    FormsModule
   ],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.scss'
