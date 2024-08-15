2ba93f523b54c8123057092845e56eba69f69d79
Vlad Romanov <vladromeur@gmail.com>
2024-08-15 15:35:59 +0500

    Завершил третью главу
    Сделал лист героев. При нажатии на героя отображаются его детали. Добавил ngFor и ngIf. Добавил новые стили для компонента heroes

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -2,20 +2,25 @@ import { Component } from '@angular/core';
 import {Hero} from "../../types/interfaces/hero.interface";
 import {UpperCasePipe} from "@angular/common";
 import {FormsModule} from "@angular/forms";
+import {HEROES} from "../../mock-heroes";
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
   imports: [
     UpperCasePipe,
-    FormsModule
+    FormsModule,
   ],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.scss'
 })
 export class HeroesComponent {
-  readonly hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
+
+  readonly heroes:Hero[] = HEROES
+
+  selectedHero?: Hero
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero
   }
 }
